767f84f9b0d7970c7bfc2dfd9ce17ec04e568fdb
Dexter Aparicio <dexter@newlogic.io>
2020-11-09 19:08:32 +0800

    genesis

---
+++ src/main/java/dx/app/MyLib2.java
@@ -0,0 +1,19 @@
+package dx.app;
+
+public class MyLib2 {
+    /**
+     * MAIN ENTRY POINT
+     * @param args Command line arguments
+     */
+    public static void main(String args[])
+    {
+        MyLib2 obj = new MyLib2();
+        int rem = obj.remainder(5,2);
+        System.out.println("remainder = " + rem);
+    }
+
+    public int remainder(int a, int b)
+    {
+        return a % b;
+    }
+}
